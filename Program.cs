97d472f449f6f96d93ad25751840249aaf5ff892
Jens Aage Arendt <jar@kombinetech.com>
2024-04-18 05:46:54 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexPortManagerPoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
