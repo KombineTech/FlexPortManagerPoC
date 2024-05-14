b7e5cfbce5d4fc1b018351744625f47c123fd765
Jens Aage Arendt <jar@kombinetech.com>
2024-05-14 15:12:55 +0200

    FlexStorage

--- Program.cs
+++ Program.cs
@@ -9,19 +9,50 @@ namespace FlexPortManagerPoC
             Kombine.Logging.Singleton.SharedLogger.Logger = new Kombine.Logging.KombineLoggerWrapper();
 
             var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var db = new FlexStorage(eUserId.FlexController, path, eTenantId.JAR, "92 43 04 95 836", "51 46 58 53 446", [1, 2, 3]);
 
-
-            db.SetState(1, eState.Door, 1);
-
-
+            FlexStorage db = new FlexStorage(eUserId.FlexController, path, eTenantId.JAR, "92 43 04 95 836", "51 46 58 53 446", [1, 2, 3]);
             db.Restore();
             _ = db.StartAsync();
+
+
+
+            db.SetUserId((int)eUserId.FlexController);
+
 
             var portManager = new PortManager();
             var units = new List<UnitXX>();
 
             Console.WriteLine("START");
+
+
+            db.SetSetting(1, eSetting.UnitType2, eUnitType.WashAllianceLog_25);
+            db.SetSetting(2, eSetting.UnitType2, eUnitType.WashAllianceLog_25);
+            db.SetSetting(3, eSetting.UnitType2, eUnitType.WashAllianceLog_25);
+            {
+                db.SetSetting(1, eSetting.FlexCount, 3);
+                db.SetSetting(1, eSetting.FlexId1, 1);
+                db.SetSetting(1, eSetting.FlexId2, 2);
+                db.SetSetting(1, eSetting.FlexId3, 3);
+            }
+
+            db.SetState(1, eState.Temperature, 2000);
+            db.SetState(1, eState.PrgTemperature, 2000);
+            db.SetState(1, eState.Level, 2000);
+            db.SetState(1, eState.PrgLevel, 2000);
+            db.SetState(1, eState.MachinStatus, 2000);
+            db.SetState(1, eState.LinenWeight, 2000);
+            db.SetState(1, eState.OutOfOrder, 2000);
+            db.SetState(1, eState.Prewash, 2000);
+            db.SetState(1, eState.PrgStep, 2000);
+            db.SetState(1, eState.Door, 2000);
+            db.SetState(1, eState.Done, 2000);
+            db.SetState(1, eState.StartedBy, 2000);
+            db.SetState(1, eState.StartedBy, 2000);
+
+            db.SetSetting(1, eSetting.Booted, "hjujklhkjhjk " + Environment.TickCount64);
+
+            Console.WriteLine(db.TestLink());
+
 
             var key = ConsoleKey.None;
             do
@@ -30,6 +61,17 @@ namespace FlexPortManagerPoC
                 key = Console.ReadKey().Key;
                 switch (key)
                 {
+
+
+
+                    case ConsoleKey.A:
+
+                        break;
+
+                    case ConsoleKey.T:
+                        db.SetState(1, eState.Temperature, Environment.TickCount);
+                        break;
+
                     case ConsoleKey.D1:
                         byte unitid = (byte)(units.Count + 1);
                         Console.WriteLine($"Add {unitid}");
